b877a48e4c3674152af6a6f62bbae3aace64731a
LeviusLuu <163327462+LeviusLuu@users.noreply.github.com>
2025-04-26 03:16:25 +0700

    save

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'my-app',
     script: 'src/index.js',  // Đảm bảo đường dẫn đến file chính của ứng dụng
-    instances: 'max',         // Tùy chỉnh số lượng instances của ứng dụng
+    instances: 1,         // Tùy chỉnh số lượng instances của ứng dụng
     exec_mode: 'cluster',     // Chạy dưới dạng cluster mode
     env: {
       NODE_ENV: 'production'
